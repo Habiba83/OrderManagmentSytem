27e9e4b1318ee6f6769dc9af6c2d60335911860f
ANTITRIX <102489946+ANTITRIX@users.noreply.github.com>
2023-12-31 00:00:49 +0200

    Add files via upload

--- OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Services/OrderService.java
+++ OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Services/OrderService.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
-
+import java.util.List;
+import java.time.Duration;
+import java.time.LocalDateTime;
 @Service
 @Scope("singleton")
 public class OrderService {
@@ -22,33 +24,46 @@ public class OrderService {
     private final Queue<String> ShippedOrdernotificationContents;
     private final Queue<String> placedOrdernotificationsContent;
 
-
     @Autowired
     private final ProductService productService;
     @Autowired
-    private final AccountService accountService;
+    private final CustomerService customerService;
 
-
-    public OrderService(ProductService productService, AccountService accountService) {
+    public OrderService(ProductService productService, CustomerService customerService) {
         this.orders = new ArrayList<>();
         this.shippingOrders = new ArrayList<>();
         this.productService = productService;
-        this.accountService = accountService;
+        this.customerService = customerService;
         this.ShippedOrdernotificationContents=new LinkedList<>();
         this.placedOrdernotificationsContent=new LinkedList<>();
-
     }
 
 
-    public String addOrder(Order order){
+    public String addOrder(Order order) {
+        List<ProductReq> products=order.getProducts();
+        // Check if the customer ID is registered
+        if (customerService.getCustomer(order.getCustomerId()) == null) {
+           return "Customer with ID " + order.getCustomerId() + " is not registered. Cannot add the order.";
+        }
+        for(ProductReq product:products){
+            if(productService.getProductBySerialNumber(product.getSerialNum())==null){
+              return"Product with serial number  " + product.getSerialNum() + "is not found";
+            }
+        }
+        // Check if the order ID is not already registered
+        if (getOrder(order.getOrderId()) != null) {
+          return "Order with ID " + order.getOrderId() + " is already registered. Cannot add the order.";
+
+        }
+        // Add the order to the list
         orders.add(order);
+        System.out.println("Order added successfully: " + order.getOrderId());
         AbstractNotificationChannel notification=new EmailChannel();
         //AbstractNotificationChannel notification=new SmsChannel(); if i want to use another channel
         notification.setOrder(order);
         String content = notification.createPlacedContent();
         placedOrdernotificationsContent.add(content);
         return content;
-
     }
 
     public Order getOrder(String orderId) {
@@ -68,10 +83,27 @@ public class OrderService {
         return shippingOrders;
     }
 
-    public boolean deleteOrder(String orderId) {
-        return orders.removeIf(order -> order.getOrderId().equals(orderId));
+    public boolean cancelOrder(String orderId) {
+        Order order = getOrder(orderId);
+        if (order != null) {
+            LocalDateTime orderPlacementTime = order.getOrderPlacementTime();
+            LocalDateTime currentTime = LocalDateTime.now();
+            Duration duration = Duration.between(orderPlacementTime, currentTime);
+            if (duration.compareTo(Duration.ofHours(2)) <= 0) {
+//                if (order.isShipped()) {
+//                    unshipOrder(orderId);
+//                }
+                orders.remove(order);
+                Customer customer = customerService.getCustomer(order.getCustomerId());
+                //customer.addBalance(order.getShippingFees());
+                System.out.println("Order canceled: " + orderId);
+                return true;
+            } else {
+                System.out.println("Cancellation period exceeded for order: " + orderId);
+            }
+        }
+        return false;
     }
-
     public Order updateOrder(String orderId , Order updatedOrder) {
         orders.removeIf(order -> order.getOrderId().equals(orderId));
         orders.add(updatedOrder);
@@ -84,7 +116,7 @@ public class OrderService {
     // =================================================
     public void shippingSimpleOrder(Order order){
         order.setShipped(true);
-        Customer customer = accountService.getCustomer(order.getCustomerId());
+        Customer customer = customerService.getCustomer(order.getCustomerId());
         customer.deductBalance(order.getShippingFees());
 
         for (ProductReq productReq : order.getProducts()) {
@@ -95,7 +127,7 @@ public class OrderService {
     }
     public void unShippingSimpleOrder(Order order){
         order.setShipped(false);
-        Customer customer = accountService.getCustomer(order.getCustomerId());
+        Customer customer = customerService.getCustomer(order.getCustomerId());
         customer.addBalance(order.getShippingFees());
 
         for (ProductReq productReq : order.getProducts()) {
@@ -136,8 +168,10 @@ public class OrderService {
         }
         AbstractNotificationChannel notification=new EmailChannel();
         notification.setOrder(order);
-        String content = notification.createPlacedContent();
+        String content = notification.createShippeedContent();
         ShippedOrdernotificationContents.add(content);
+
+
 
         return content;
     }
@@ -156,10 +190,15 @@ public class OrderService {
             System.out.println("Shipping the order..." + orderId);
         }
 
-
         shippingOrders.removeIf(order1 -> order1.getOrderId().equals(orderId));
+
+
         return "cancel shipping the order..."+ orderId;
     }
-
-
+    public Queue<String> getAllShippingNotifications(){
+        return ShippedOrdernotificationContents;
+    }
+    public Queue<String> getAllPlacingNotifications(){
+        return placedOrdernotificationsContent;
+    }
 }
