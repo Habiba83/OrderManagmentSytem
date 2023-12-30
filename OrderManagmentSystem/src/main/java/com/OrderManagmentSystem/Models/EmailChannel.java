35ddedd4ed461f8689c0732d1bb09bc2a6420f45
ANTITRIX <102489946+ANTITRIX@users.noreply.github.com>
2023-12-30 23:58:01 +0200

    Add files via upload

--- OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Models/EmailChannel.java
+++ OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Models/EmailChannel.java
@@ -9,13 +9,13 @@ public class EmailChannel extends AbstractNotificationChannel {
 
     @Override
     public String createPlacedContent() {
-        this.setContent("dear "+ order.getCustomerId()+" your booking of the "+ order.getOrderId()+" is confirmed. thanks for using our store :)");
+        this.setContent("dear "+ order.getCustomerId()+" your booking of the order : "+ order.getProductsNames()+" is confirmed. thanks for using our store :)");
         return this.content;
     }
 
     @Override
     public String createShippeedContent() {
-        this.setContent("dear "+ order.getCustomerId()+" your "+ order.getOrderId()+" has arrived to your address sucessfully !");
+        this.setContent("dear "+ order.getCustomerId()+" your order : "+ order.getProductsNames()+" is on it's way to your address !");
         return this.content;
     }
 }
--- OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Models/SmsChannel.java
+++ OrderManagmentSystem/src/main/java/com/OrderManagmentSystem/Models/SmsChannel.java
@@ -3,7 +3,7 @@ package com.OrderManagmentSystem.Models;
 public class SmsChannel extends AbstractNotificationChannel {
     @Override
     public String createShippeedContent() {
-        this.setContent("dear "+ order.getCustomerId()+" your booking of the "+ order.getOrderId()+" is confirmed. thanks for using our store :)");
+        this.setContent("dear "+ order.getCustomerId()+" your booking of the order : "+ order.getProductsNames()+" is confirmed. thanks for using our store :)");
         return this.content;
     }
 
